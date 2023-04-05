a6a0c8f443a3afad78615a4a05b8b39a8d90622d
pohlebkin <pohlebkin@gmail.com>
2023-04-05 17:31:16 +0500

    список постов, работающая форма добавления поста
    видео урок сейчас на 31:30

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
-<script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+export default {
+    data() {
+        return {
+            posts: [
+                { id: 1, title: 'title1', body: 'body1' },
+                { id: 2, title: 'title2', body: 'body2' },
+                { id: 3, title: 'title3', body: 'body3' },
+            ],
+            title: '',
+            body: '',
+        }
+    },
+    methods: {
+        creatrePost() {
+            this.posts.push({
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            })
+            this.title = ''
+            this.body = ''
+        },
+    },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div class="app">
+        <form @submit.prevent>
+            <h4>создание поста</h4>
+            <input
+                v-model="title"
+                class="input"
+                type="text"
+                placeholder="название"
+            />
+            <input
+                v-model="body"
+                class="input"
+                type="text"
+                placeholder="описание"
+            />
+            <button class="btn" @click="creatrePost">добавить</button>
+        </form>
+        <div>
+            <div class="post" v-for="post in posts">
+                <div><b>название</b> {{ post.title }}</div>
+                <div><b>описание</b> {{ post.body }}</div>
+            </div>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
