932d95de55da2e778f8b8c32fc90b791c2546d92
pohlebkin <pohlebkin@gmail.com>
2023-04-07 08:53:21 +0500

    анимация добавления, удаления, сортировки
    https://v3.ru.vuejs.org/ru/guide/transitions-list.html
    
    1:39:02 - 1:42:17

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -18,12 +18,36 @@ export default {
 <template>
     <div v-if="posts.length > 0">
         <h2>посты</h2>
-        <PostItem
-            @remove="$emit('remove', post)"
-            :key="post.id"
-            :post="post"
-            v-for="post in posts"
-        />
+        <transition-group name="post-list">
+            <PostItem
+                @remove="$emit('remove', post)"
+                :key="post.id"
+                :post="post"
+                v-for="post in posts"
+            />
+        </transition-group>
     </div>
     <h2 v-else style="color: red">постов нет</h2>
 </template>
+
+<style scoped>
+.post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+    transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+.post-list-move {
+    transition: all 0.4s ease;
+}
+</style>
